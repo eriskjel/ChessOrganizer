e64a7111030dc43857b005917c77ece1f768ea14
Navid <Navidm@stud.ntnu.no>
2022-03-22 19:04:01 +0100

    Added getters and setters to Match class

--- idatt1002_2022_k2g9/src/main/java/ntnu/idatt2001/k2g9/Match.java
+++ idatt1002_2022_k2g9/src/main/java/ntnu/idatt2001/k2g9/Match.java
@@ -3,15 +3,34 @@ package ntnu.idatt2001.k2g9;
 public class Match {
     private Player player1;
     private Player player2;
-    private int result;
+    private Player winner;
 
     public Match(Player player1, Player player2) {
         this.player1 = player1;
         this.player2 = player2;
     }
 
-    public void setResult(int result) {
-        this.result = result;
+    public void setWinner(Player winner) {
+        this.winner = winner;
     }
 
+    public Player getPlayer1() {
+        return player1;
+    }
+
+    public void setPlayer1(Player player1) {
+        this.player1 = player1;
+    }
+
+    public Player getPlayer2() {
+        return player2;
+    }
+
+    public void setPlayer2(Player player2) {
+        this.player2 = player2;
+    }
+
+    public Player getWinner() {
+        return winner;
+    }
 }
