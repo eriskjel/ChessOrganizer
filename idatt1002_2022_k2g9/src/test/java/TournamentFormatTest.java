a67efa8209167752432acd73e2271713e899a599
Navid <Navidm@stud.ntnu.no>
2022-03-23 11:27:03 +0100

    Created test for TournamentFormat class

--- idatt1002_2022_k2g9/src/test/java/TournamentFormatTest.java
+++ idatt1002_2022_k2g9/src/test/java/TournamentFormatTest.java
@@ -2,6 +2,7 @@ import ntnu.idatt2001.k2g9.Match;
 import ntnu.idatt2001.k2g9.Player;
 import ntnu.idatt2001.k2g9.PlayerRegistry;
 import ntnu.idatt2001.k2g9.TournamentFormat;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -12,7 +13,7 @@ import java.util.ArrayList;
  */
 class TournamentFormatTest {
     @Test
-    void tournamentFormatCreatesCorrect() {
+    void knockOutLayoutIsAsExpected() {
         PlayerRegistry players = new PlayerRegistry();
 
         for(int i = 0 ; i<20 ; i++){
@@ -21,15 +22,34 @@ class TournamentFormatTest {
 
         ArrayList<Match[]> layout = TournamentFormat.createRounds("Knock-Out", players);
 
-        for(int i = 0 ; i<layout.size() ; i++){
-            for (Match match : layout.get(i)){
-                try {
-                    System.out.println(match.toString());
-                }
-                catch(NullPointerException ex){
-
-                }
-            }
-        }
+        Assertions.assertTrue(layout.get(0).length == 4
+                                & layout.get(1).length == 8
+                                & layout.get(2).length == 4
+                                & layout.get(3).length == 2
+                                & layout.get(4).length == 1);
     }
+
+    @Test
+    void knockOutBracketFilledCorrectly() {
+        PlayerRegistry players = new PlayerRegistry();
+
+        for(int i = 0 ; i<20 ; i++){
+            players.addPlayer(new Player("eriskjel@ntnu.no","456"+Integer.toString(i),"Erik"+Integer.toString(i), 300,20,"Dirt",0,100));
+        }
+
+        ArrayList<Match[]> layout = TournamentFormat.createRounds("Knock-Out", players);
+
+        int playersPlacedInPreliminary = 0;
+        for(int i = 0 ; i < layout.get(0).length ; i++){
+            playersPlacedInPreliminary += layout.get(0)[i].playersInitialized();
+        }
+
+        int playersPlacedInFirstRound = 0;
+        for(int i = layout.get(1).length-1 ; i >= playersPlacedInPreliminary/4 ; i--){
+            playersPlacedInFirstRound += layout.get(1)[i].playersInitialized();
+        }
+
+        Assertions.assertTrue(playersPlacedInPreliminary == 8 && playersPlacedInFirstRound == 12);
+    }
+
 }
