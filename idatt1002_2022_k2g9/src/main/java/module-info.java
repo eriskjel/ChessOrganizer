ad1bb118e24d49858bb54776dfadf0ebda625cda
daniesky <daniesky@stud.ntnu.no>
2022-03-24 13:30:43 +0100

    updated module-info

--- idatt1002_2022_k2g9/src/main/java/module-info.java
+++ idatt1002_2022_k2g9/src/main/java/module-info.java
@@ -9,4 +9,8 @@ module ChessTournament {
     //opens ntnu.idatt2001.k2g9 to javafx.fxml;
     exports ntnu.idatt2001.k2g9.gui;
     opens ntnu.idatt2001.k2g9.gui to javafx.fxml;
+    exports ntnu.idatt2001.k2g9.player;
+    opens ntnu.idatt2001.k2g9.player to javafx.fxml;
+    exports ntnu.idatt2001.k2g9.tournament;
+    opens ntnu.idatt2001.k2g9.tournament to javafx.fxml;
 }
