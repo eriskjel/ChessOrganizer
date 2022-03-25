62825604ca139f67e3712ede6dc7c8c4223714b3
Edvard <ecschoye@stud.ntnu.no>
2022-03-25 15:10:37 +0100

    added tests to round robin in TournamentFormatTest

--- idatt1002_2022_k2g9/src/test/java/TournamentFormatTest.java
+++ idatt1002_2022_k2g9/src/test/java/TournamentFormatTest.java
@@ -56,13 +56,27 @@ class TournamentFormatTest {
     void roundRobinLayoutIsAsExpected() {
         PlayerRegistry players = new PlayerRegistry();
 
-        for(int i = 0 ; i<6 ; i++){
+        for(int i = 0 ; i<7 ; i++){
             players.addPlayerObject(new Player("Erik"+Integer.toString(i+1),20+i));
         }
 
-        ArrayList<Match[]> layout = TournamentFormat.createBracket("Round-robin", players);
+        ArrayList<Match[]> layout = TournamentFormat.createBracket("Round-Robin", players);
 
+        //test to see how many rounds will be played
+        //should be participants.size()-1, which is 7 with 7 competitors + 1 dummy "Bye"
+        Assertions.assertEquals(layout.size(),7);
+        Assertions.assertNotEquals(layout.size(),8);
+        Assertions.assertNotEquals(layout.size(),5);
 
+        //test to see how many matches there'll be in each round
+        //should be participants.size()/2, which is 4 since there's 7 competitors + 1 dummy "Bye"
+        Assertions.assertTrue(layout.get(0).length == 4
+                                    && layout.get(1).length == 4
+                                    && layout.get(2).length == 4
+                                    && layout.get(3).length == 4
+                                    && layout.get(4).length == 4
+                                    && layout.get(5).length == 4
+                                    && layout.get(6).length == 4);
     }
 
     @Test
@@ -74,5 +88,18 @@ class TournamentFormatTest {
         }
 
         ArrayList<Match[]> layout = TournamentFormat.createBracket("Round-Robin" , players);
+
+        int playersInRoundOne = 0;
+        for (int i = 0; i <layout.get(0).length ; i++) {
+            playersInRoundOne += layout.get(0)[i].playersInitialized();
+        }
+
+        int playersInRoundTwo = 0;
+        for (int i = 0; i <layout.get(0).length ; i++) {
+            playersInRoundTwo += layout.get(1)[i].playersInitialized();
+        }
+
+        Assertions.assertEquals(playersInRoundOne, playersInRoundTwo);
+
     }
 }
