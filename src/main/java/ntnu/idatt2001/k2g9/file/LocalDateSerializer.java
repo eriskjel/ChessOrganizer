7bd050bd417ca986e141b915840ae6ddf85eb389
Daniel Skymoen <daniesky@stud.ntnu.no>
2022-04-06 15:43:55 +0200

    Created deserializer for the LocalDate class

---
+++ src/main/java/ntnu/idatt2001/k2g9/file/LocalDateSerializer.java
@@ -0,0 +1,21 @@
+package ntnu.idatt2001.k2g9.file;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class LocalDateSerializer extends StdSerializer<LocalDate>{
+    public LocalDateSerializer(){
+        super(LocalDate.class);
+    }
+
+    @Override
+    public void serialize(LocalDate value, JsonGenerator gen, SerializerProvider sp) throws IOException, JsonProcessingException, IOException {
+        gen.writeString(value.format(DateTimeFormatter.ISO_LOCAL_DATE));
+    }
+}
