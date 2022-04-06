d748929102a83b044998e328f274957c0c764a3a
Erik Skjellevik <eriskjel@stud.ntnu.no>
2022-04-06 11:40:46 +0200

    added filehandler class

---
+++ src/main/java/ntnu/idatt2001/k2g9/file/FileHandler.java
@@ -0,0 +1,30 @@
+package ntnu.idatt2001.k2g9.file;
+
+import ntnu.idatt2001.k2g9.gui.models.CompetitorModel;
+import ntnu.idatt2001.k2g9.player.Player;
+import ntnu.idatt2001.k2g9.tournament.Tournament;
+
+public class FileHandler {
+
+
+
+
+    public Tournament readTournamentFromFile(){
+        //dd
+    }
+
+
+    public void writeTournamentToFile(){
+        //dd
+    }
+
+    public Player readCompetitorFromFile(){
+        //dd
+    }
+
+    public void writeCompetitorToFile(){
+        //fd
+    }
+
+}
+
