5b89360606b82eeed61a5654dfbcc424770ef160
Navid <Navidm@stud.ntnu.no>
2022-04-06 17:17:11 +0200

    Fixed incorrect deserialization of tournamentBracket

--- src/main/java/ntnu/idatt2001/k2g9/file/TournamentDeserializer.java
+++ src/main/java/ntnu/idatt2001/k2g9/file/TournamentDeserializer.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import ntnu.idatt2001.k2g9.player.Admin;
 import ntnu.idatt2001.k2g9.player.Player;
 import ntnu.idatt2001.k2g9.player.PlayerRegistry;
@@ -38,9 +39,13 @@ public class TournamentDeserializer extends StdDeserializer{
         int id = (Integer) node.get("tournamentID").numberValue();
         String date = mapper.convertValue(node.get("date"),String.class);
         Admin admin = mapper.convertValue(node.get("admin"),Admin.class);
-        ArrayList<Match[]> bracket  = mapper.convertValue(node.get("tournamentBracket"),ArrayList.class);
-        JsonNode pNode = node.get("playerRegistry");
-        PlayerRegistry players = mapper.readValue(pNode.traverse(mapper), PlayerRegistry.class);
+        ArrayList<Match[]> bracket  = new ArrayList<>();
+        for (JsonNode matchArrayNode : node.get("tournamentBracket")) {
+            bracket.add(mapper.convertValue(matchArrayNode , Match[].class));
+        }
+        PlayerRegistry players = mapper.convertValue(node.get("playerRegistry"),PlayerRegistry.class);
+
+        //PlayerRegistry players1 = mapper.readValue(pNode.traverse(mapper), PlayerRegistry.class);
         return new Tournament(name,LocalDate.parse(date),bracket,layout,admin,id,players);
     }
 }
