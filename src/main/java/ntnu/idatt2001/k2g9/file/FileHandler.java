1837a51b8da12d40afb20a9f5c4a2ff2b281b674
Daniel Skymoen <daniesky@stud.ntnu.no>
2022-04-22 13:59:02 +0200

    Changed file handler class so that it now interacts with only one file. Can write, read and remove from said file.

--- src/main/java/ntnu/idatt2001/k2g9/file/FileHandler.java
+++ src/main/java/ntnu/idatt2001/k2g9/file/FileHandler.java
@@ -1,5 +1,6 @@
 package ntnu.idatt2001.k2g9.file;
 
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.json.JsonMapper;
@@ -7,40 +8,72 @@ import com.fasterxml.jackson.databind.util.JSONPObject;
 import ntnu.idatt2001.k2g9.player.Player;
 import ntnu.idatt2001.k2g9.tournament.Tournament;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class FileHandler {
 
-    ObjectMapper mapper = new ObjectMapper();
-    private String defaultPath = "src/main/resources/ntnu/idatt2001/k2g9/gui/registry/tournaments/";
+    ObjectMapper mapper;
+    private String defaultPath;
+
+    public FileHandler(){
+        this.defaultPath = "src/main/resources/ntnu/idatt2001/k2g9/gui/registry/tournaments/Tournaments.json";
+        this.mapper = new ObjectMapper();
+    }
+
+    public FileHandler(String path){
+        this.defaultPath = path;
+        this.mapper = new ObjectMapper();
+    }
 
 
 
-    public Tournament readTournamentFromFile(String fileName) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        return mapper.readValue(new File(defaultPath + fileName), Tournament.class);
+    public Tournament readTournamentFromFile(int tournamentID) throws IOException, ParseException {
+        JSONParser jsonParser = new JSONParser();
+        //Reads the JSONArray in the file.
+        JSONArray jsonArray = (JSONArray) jsonParser.parse(new FileReader(defaultPath));
+        //Retrieves jsonObject within the jsonArray, which contains tournaments.
+        JSONObject jsonObject = (JSONObject) jsonArray.get(tournamentID);
+        String jsonValue = (String)jsonObject.get(String.valueOf(tournamentID));
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Tournament t = objectMapper.readValue(jsonValue,Tournament.class);
+        return t;
 
     }
 
 
-    public void writeTournamentToFile(Tournament tournament) throws IOException {
-        mapper.writeValue(new File(defaultPath + this.getTournamentFilePath(tournament).replaceAll(" ", "")), tournament);
+    public void writeTournamentToFile(Tournament tournament) throws IOException, ParseException {
+        JSONParser jsonParser = new JSONParser();
+
+        //Reads the Array that exists in the tournament file.
+        JSONArray jsonArray = (JSONArray) jsonParser.parse(new FileReader(defaultPath));
+
+        //Creates a json string formatted properly using serializer.
+        String jsonString = mapper.writeValueAsString(tournament);
+
+        //Create a json object with the formatted string and a key, which is assigned by tournamentID.
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put(tournament.getTournamentID(),jsonString);
+        jsonArray.add(jsonObject);
+
+        //Overwrites the Json file with updated JSONArray.
+        mapper.writeValue(new File(defaultPath),jsonArray);
     }
 
-    public Player readCompetitorFromFile(){
-        return null;
+    public void removeTournament(int tournamentID) throws IOException, ParseException {
+        JSONParser jsonParser = new JSONParser();
+        //Reads the JSONArray in the file.
+        JSONArray jsonArray = (JSONArray) jsonParser.parse(new FileReader(defaultPath));
+        jsonArray.remove(tournamentID);
+        mapper.writeValue(new File(defaultPath),jsonArray);
     }
-
-    public void writeCompetitorToFile(){
-        //fd
-    }
-
-    public String getTournamentFilePath(Tournament tournament){
-        return tournament.getName() + tournament.getTournamentID() + ".json";
-    }
-
-
 }
 
--- src/test/java/ntnu/idatt2001/k2g9/file/FileHandlerTest.java
+++ src/test/java/ntnu/idatt2001/k2g9/file/FileHandlerTest.java
@@ -1,13 +1,22 @@
 package ntnu.idatt2001.k2g9.file;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import ntnu.idatt2001.k2g9.player.Player;
 import ntnu.idatt2001.k2g9.player.PlayerRegistry;
 import ntnu.idatt2001.k2g9.tournament.Tournament;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.beans.Transient;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.time.LocalDate;
 
@@ -15,77 +24,109 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class FileHandlerTest {
 
+    public String testPath = "src/main/resources/ntnu/idatt2001/k2g9/gui/registry/tournaments/TournamentsTest.json";
+
     @Test
-    public void writeTournamentToFile() throws IOException {
-
+    public void writeAndReadTest() throws IOException, ParseException {
+        //Initialize a PlayerRegistry with 5 players to use in the tournament.
         PlayerRegistry players = new PlayerRegistry();
-
         for(int i = 0 ; i<5 ; i++){
             players.addPlayer(new Player("Test" + i,20+i));
         }
 
+        // Initialize a tournament with the players, the current date and a specific name.
         Tournament tournament = new Tournament("TournamentTestOne", LocalDate.now(), "Knock-Out");
         tournament.setPlayers(players);
         tournament.createTournamentBracket();
 
-        FileHandler fileHandler = new FileHandler();
+        // Create a fileHandler with the specified path to the file used for test writing and reading.
+        FileHandler fileHandler = new FileHandler(testPath);
         fileHandler.writeTournamentToFile(tournament);
 
-        String name = tournament.getName() + tournament.getTournamentID() + ".json";
-        File file = new File("src/main/resources/ntnu/idatt2001/k2g9/gui/registry/tournaments/" + name);
+        // We use the id from the tournament created above to locate the tournament in the JSON file.
+        Tournament read = fileHandler.readTournamentFromFile(tournament.getTournamentID());
 
-        assertTrue(file.exists());
-        //deletes files after test
-        file.delete();
+        //Confirms that the tournament was written correctly.
+        Assertions.assertEquals(tournament,read);
 
-
-
+        //Run the test for two tournaments, to assure that several tournaments wont cause issues.
         Tournament tournament1 = new Tournament("TournamentTestTwo", LocalDate.now(), "Swiss-System");
         tournament1.setPlayers(players);
         tournament1.createTournamentBracket();
+        tournament1.setTournamentID(1);
 
         fileHandler.writeTournamentToFile(tournament1);
+        Tournament read1 = fileHandler.readTournamentFromFile(tournament1.getTournamentID());
+        Assertions.assertEquals(tournament1,read1);
 
-        String name1 = tournament1.getName() + tournament1.getTournamentID() + ".json";
-        File file1 = new File("src/main/resources/ntnu/idatt2001/k2g9/gui/registry/tournaments/" + name1);
-
-
-        assertTrue(file1.exists());
-        //delete file after test
-        file1.delete();
+        //Overwrites the test file with an empty JSONArray after testing is done.
+        ObjectMapper objectMapper = new ObjectMapper();
+        JSONArray jsonArray = new JSONArray();
+        objectMapper.writeValue(new File(testPath),jsonArray);
 
 
     }
 
-
     @Test
-    public void readFromFile() throws IOException {
-        PlayerRegistry players = new PlayerRegistry();
 
+    public void removeTest() throws IOException, ParseException {
+        //Initialize a PlayerRegistry with 5 players to use in the tournament.
+        PlayerRegistry players = new PlayerRegistry();
+        for(int i = 0 ; i<5 ; i++){
+            players.addPlayer(new Player("Test" + i,20+i));
+        }
+
+        // Initialize a tournament with the players, the current date and a specific name.
+        Tournament tournament = new Tournament("TournamentTestOne", LocalDate.now(), "Knock-Out");
+        tournament.setPlayers(players);
+        tournament.createTournamentBracket();
+
+        // Create a fileHandler with the specified path to the file used for test writing and reading.
+        FileHandler fileHandler = new FileHandler(testPath);
+        fileHandler.writeTournamentToFile(tournament);
+
+        // We use the id from the tournament created above to locate the tournament in the JSON file.
+        Tournament read = fileHandler.readTournamentFromFile(tournament.getTournamentID());
+
+        //Confirms that the tournament was written correctly.
+        Assertions.assertEquals(tournament,read);
+
+        fileHandler.removeTournament(tournament.getTournamentID());
+
+        Assertions.assertThrows(IndexOutOfBoundsException.class ,() -> fileHandler.readTournamentFromFile(tournament.getTournamentID()));
+
+    }
+    //Testene er unødvendig?
+/*
+    @Test
+    public void readFromFile() throws IOException, ParseException {
+        //Initialize a PlayerRegistry with 5 players to use in the tournament.
+        PlayerRegistry players = new PlayerRegistry();
         for(int i = 0 ; i < 5 ; i++){
             players.addPlayer(new Player("Test"+ Integer.toString(i),20+i));
         }
 
+        //Create a tournament with the players above.
         Tournament tournament = new Tournament("TournamentTestOne", LocalDate.now(), "Knock-Out");
         tournament.setPlayers(players);
         tournament.createTournamentBracket();
 
-
-
-        FileHandler fileHandler = new FileHandler();
+        //Filehandler object created with the testpath.
+        FileHandler fileHandler = new FileHandler(testPath);
         fileHandler.writeTournamentToFile(tournament);
 
+        //Confirms that the tournament has been read properly.
+        assertEquals(tournament, fileHandler.readTournamentFromFile(0));
 
-        assertEquals(tournament, fileHandler.readTournamentFromFile(fileHandler.getTournamentFilePath(tournament)));
-
-        //deletes file after use
-        File file = new File("src/main/resources/ntnu/idatt2001/k2g9/gui/registry/tournaments/" + fileHandler.getTournamentFilePath(tournament));
-        file.delete();
+        //Overwrites the test file with an empty JSONArray after testing is done.
+        ObjectMapper objectMapper = new ObjectMapper();
+        JSONArray jsonArray = new JSONArray();
+        objectMapper.writeValue(new File(testPath),jsonArray);
     }
 
 
     @Test
-    public void readAndWriteToFile() throws IOException {
+    public void readAndWriteToFile() throws IOException, ParseException {
         PlayerRegistry players = new PlayerRegistry();
 
         for(int i = 0 ; i<5 ; i++){
@@ -96,19 +137,19 @@ public class FileHandlerTest {
         tournament.setPlayers(players);
         tournament.createTournamentBracket();
 
-        FileHandler fileHandler = new FileHandler();
+        FileHandler fileHandler = new FileHandler(testPath);
         fileHandler.writeTournamentToFile(tournament);
 
-
-        String name = tournament.getName() + tournament.getTournamentID() + ".json";
-        File file = new File("src/main/resources/ntnu/idatt2001/k2g9/gui/registry/tournaments/" + name);
-        assertTrue(file.exists());
-
-        Tournament sameTournamentFromFile = fileHandler.readTournamentFromFile(name);
+        Tournament sameTournamentFromFile = fileHandler.readTournamentFromFile(tournament.getTournamentID());
         assertEquals(tournament, sameTournamentFromFile);
 
-        file.delete();
+        //Overwrites the test file with an empty JSONArray after testing is done.
+        ObjectMapper objectMapper = new ObjectMapper();
+        JSONArray jsonArray = new JSONArray();
+        objectMapper.writeValue(new File(testPath),jsonArray);
 
     }
+
+ */
 
 }
